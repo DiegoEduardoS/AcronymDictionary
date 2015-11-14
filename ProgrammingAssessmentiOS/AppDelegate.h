aff2fd486e0950a434d58bf6ed30de9c84932378
Diego Eduardo <mcs@mcss-macbook-air.local>
2015-11-13 19:10:30 -0500

    Initial Commit

---
+++ ProgrammingAssessmentiOS/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ProgrammingAssessmentiOS
+//
+//  Created by MCS on 11/13/15.
+//  Copyright © 2015 MCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ProgrammingAssessmentiOS/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ProgrammingAssessmentiOS
+//
+//  Created by MCS on 11/13/15.
+//  Copyright © 2015 MCS. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
